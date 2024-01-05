99b9bc4c599022045c9fd3289f77a7dce1783c44
Abhishek Tiwari ☕ <97331593+iamabhish23@users.noreply.github.com>
2024-01-05 11:13:15 +0530

    Create ReverseArray.java

---
+++ Array/ReverseArray.java
@@ -0,0 +1,15 @@
+class Reverse
+{
+    // Complete the function
+    // str: input string
+    public static String reverseWord(String str)
+    {
+        // Reverse the string str
+        StringBuilder ans = new StringBuilder();
+        for(int i=str.length()-1; i>=0; i--){
+            ans.append(str.charAt(i));
+            
+        }
+        return ans.toString();
+    }
+}
