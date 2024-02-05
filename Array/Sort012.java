3d1fa8dbf91e5a28c1fd8fc16fe55452fb38e6b9
Abhishek Tiwari <iamabhishek.tiwari23@gmail.com>
2024-02-06 01:11:37 +0530

    done

--- Array/Sort012.java
+++ Array/Sort012.java
@@ -2,9 +2,35 @@ public class Sort012 {
     public static void main(String[] args) {
         int N = 5;
         int arr[]= {0, 2, 1, 2, 0};
-        sort012(arr, N);
+        sorts012(arr, N);
         print(arr);
         
+    }
+    public static void swap(int arr[], int i, int j){
+        int temp = arr[i];
+        arr[i]= arr[j];
+        arr[j]=temp;
+
+    }
+
+    public static void sorts012(int a[], int n)
+    {
+        // code here 
+        int current = 0; 
+        int high = a.length-1;
+        int low = 0;
+        while(current<=high){
+            if(a[current]==0){
+                swap(a, current, low); 
+                low++;
+                current++;
+            }else if(a[current]==2){
+                swap(a, high, current);
+                high--;
+            }else{
+                current++;
+            }
+        }
     }
 
     public static void print(int arr[]){
